e95ec75d598e1185f76f5b78aa2d65c637d64112
OJIMA Ryoji <rojima1@gmail.com>
2020-03-16 20:26:56 +0900

    refactored.

--- app/src/main/java/com/tail_island/jetbus/BookmarksFragment.kt
+++ app/src/main/java/com/tail_island/jetbus/BookmarksFragment.kt
@@ -1,10 +1,10 @@
 package com.tail_island.jetbus
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.tail_island.jetbus.databinding.FragmentBookmarksBinding
 
